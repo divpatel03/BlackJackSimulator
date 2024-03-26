ceee25dd8b40b1ff3d821cc1e31a628b4dbbecd5
Divya Patel <142425083+divpatel03@users.noreply.github.com>
2024-03-26 12:27:03 -0400

    Added a small print sentence

--- GamePrint.java
+++ GamePrint.java
@@ -49,6 +49,7 @@ public class GamePrint {
                         "\r\n" + //
                         "Card Values/scoring\r\n" + //
                         "It is up to each individual player if an ace is worth 1 or 11. Face cards are 10 and any other card is its pip value.");
+        System.out.println("Ready to Go");
         System.out.println("--------------------------------------");
         System.out.println();
     }
