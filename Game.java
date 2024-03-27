72e654fced6a56c783131778f9093154b70a8a19
Divya Patel <divpatel031000@gmail.com>
2024-03-26 23:27:14 -0400

    Replaced the nested if statements to a loop to keep prompting the user

--- Game.java
+++ Game.java
@@ -41,47 +41,37 @@ public class Game{
         //Running hit or stand method and asking the user wheter they want to hit/stand
         hitOrStandValue = gp1.hitOrStand();
         
-        //If player decides to hit (First)
-        if(hitOrStandValue == 1) {
-            playerHand += gl1.hit(playerHandValue);
-            //If the player wins after the first hit, display the win message
-            if(playerHand == 21) {
-                System.out.println("21! You Won!");
-            }
-            //If the player loses after the first hit, display the lost message
-            if(playerHand > 21) {
-                System.out.println("Bust! You Lost!");
-            }
-            //If they didn't win or lose, prompt them again to hit or stand
-            else {
-                //Reset value back to 0
-                hitOrStandValue = 0;
-                hitOrStandValue = gp1.hitOrStand();
+        // Assume hitOrStandValue is set before entering the loop
 
-                //If they stand (Second)
-                if(hitOrStandValue == 0) {
-                    gl1.stand();
+        while (playerHand < 21) {
+            //If player decides to hit
+            if (hitOrStandValue == 1) {
+                playerHand += gl1.hit(playerHandValue);
+                System.out.println("Player Hand Total: " + playerHand);
+                //If the player wins after the first hit, display the win message
+                if (playerHand == 21) {
+                    System.out.println("21! You Won!");
+                    break; // Exit the loop since the player won
                 }
-                //If they hit (Second)
+                //If the player loses after the first hit, display the lost message
+                else if (playerHand > 21) {
+                    System.out.println("Bust! You Lost!");
+                    break; // Exit the loop since the player lost
+                }
+                //If they didn't win or lose, prompt them again to hit or stand
                 else {
-                    //Add the new card draw to the total hand
-                    playerHand += gl1.hit(playerHandValue);
-                    //Display the total hand
-                    System.out.println("Player Hand Total: " +playerHand);
-                    //If it is over 21, display lost message
-                    if(playerHand > 21) {
-                        System.out.println("Bust! You Lost!");
-                    }
-                    //If the player wins after the second hit
-                    if(playerHand == 21) {
-                        System.out.println("21! You Won!");
-                    }
+                    hitOrStandValue = gp1.hitOrStand(); // Prompt for hit or stand
                 }
+            }
+            //If they stand
+            else if (hitOrStandValue == 0) {
+                gl1.stand();
+                break; // Exit the loop as the player chose to stand
             }
         }
-        //If player decides to stand
-        else {
-            gl1.stand();
+
+        if(hitOrStandValue == 0) {
+            
         }
 
     }
